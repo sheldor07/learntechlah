aefae678a2e28823631087f4ef108c24e32b6ff8
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-01-15 16:32:13 +0800

    added Users, Projects model defns
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+
+const {db} = require('./src/db/models.js')
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/',express.static(__dirname + '/src/public'))
+
+
+db.sync()
+    .then(()=>{
+        app.listen(8484,()=>{
+            console.log('server started on localhost:8484')
+        })
+    })
+    .catch((err) => {
+        console.error(new Error('Could not start database'))
+        console.error(err)
+      })
+
---
+++ src/db/models.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize')
+//initialising database
+const db = new Sequelize({
+    dialect: 'mysql',
+    database: 'techlinkedindb',
+    username: 'collegeuser',
+    password: 'collegepass',
+})
+// standard definitions for attributes
+const COL_USERNAME_DEF = {
+    type: Sequelize.DataTypes.STRING(100),
+    unique: true,
+    allowNull: false
+}
+
+//tables in db
+const Users = db.define('user', {
+    username: COL_USERNAME_DEF,
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        set(value) {
+            // Storing passwords in plaintext in the database is terrible.
+            // Hashing the value with an appropriate cryptographic hash function is better.
+            this.setDataValue('password', hash(value));
+        }
+    },
+    emailId:Sequelize.DataTypes.STRING,
+    track:Sequelize.DataTypes.STRING,
+    progress:{
+        type:Sequelize.DataTypes.INTEGER,
+    }
+})
+
+const Projects = db.define('project', {
+    name:Sequelize.DataTypes.STRING,
+    description:Sequelize.DataTypes.STRING,
+    githubLink:Sequelize.DataTypes.STRING,
+    demoLink:Sequelize.DataTypes.STRING,
+    techStack:Sequelize.DataTypes.STRING,
+})
+
+Users.hasMany(Projects)
+Projects.belongsTo(Users)
+
+module.exports = {
+    db, 
+    Users,
+    Projects
+}
